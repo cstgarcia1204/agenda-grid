cf83aaf4f771cdd83a185aaa6131b1becc2c7edb
cstgarcia <ckstgarcia@gmail.com>
2022-10-13 04:11:49 -0600

    add nvm file, add dependencies, use of ag-grid, display data from json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import CLIENTCOLUMNS from '../app/clientData.json';  
+import COLUMNDEFS from '../app/colDef.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agenda';
+  title = 'Directory';
+  
+  // Each Column Definition results in one Column.
+  colDefs = COLUMNDEFS;
+  rowData = CLIENTCOLUMNS;
+  // DefaultColDef sets props common to all Columns
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  @ViewChild('myGrid') grid!: AgGridAngular;
+
+  // onGridReady(params: GridReadyEvent) {
+  // }
+  
+
+ 
+  
+
+  
+
+  // onCellClicked( e: CellClickedEvent): void {
+  //   console.log('cellClicked', e);
+  // }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AgGridModule,
     AppRoutingModule
   ],
   providers: [],
